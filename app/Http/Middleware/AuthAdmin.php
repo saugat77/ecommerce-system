c390d196a3255a2ad1015b99fa6a206b6980a8b8
= <saugatpandey4@gmail.com>
2022-10-08 11:06:06 +0545

    changes for issue in admin dashboard fixed

--- app/Http/Middleware/AuthAdmin.php
+++ app/Http/Middleware/AuthAdmin.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class AuthAdmin
@@ -16,7 +17,7 @@ class AuthAdmin
      */
     public function handle(Request $request, Closure $next)
     {
-        if(session('utype')==='ADM')
+        if(Auth::user()->utype === 'ADM')
         {
             return $next($request);
         }
